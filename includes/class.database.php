eee84cb6c7d44a65fe17e4c2443d3f6625741819
KohaJeseMen <kohajesemen@yahoo.com>
2017-02-25 03:06:02 +0700

    Create class.database.php

---
+++ includes/class.database.php
@@ -0,0 +1,19 @@
+<?php
+ /**
+ * @project   FotoSystem
+ * @name      PHP Mysqli Database Class
+ * @category  Database Access
+ * @package   MySQLiDb
+ * @author    Nguyễn Hoàng Anh Khoa <kohajesemen@gmail.com>
+ * @copyright Copyright (c) 2017
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link      http://github.com/FotoSystem/includes/class.database.php
+ * @version   1.0-master
+ * @method    connect()
+ */
+ 
+  class database{
+    // Class Database;
+    
+  }
+?>
