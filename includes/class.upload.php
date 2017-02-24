2b10650500e6e868324a877fc88856e6602d8027
KohaJeseMen <kohajesemen@yahoo.com>
2017-02-25 03:07:40 +0700

    Update class.upload.php

--- includes/class.upload.php
+++ includes/class.upload.php
@@ -1,4 +1,17 @@
 <?php
+  /**
+ * @project   FotoSystem
+ * @name      PHP Mysqli Database Class
+ * @category  Database Access
+ * @package   MySQLiDb
+ * @author    Nguyễn Hoàng Anh Khoa <kohajesemen@gmail.com>
+ * @copyright Copyright (c) 2017
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link      http://github.com/FotoSystem/includes/class.database.php
+ * @version   1.0-master
+ * @method    upload()
+ */
+
   class upload{
   // Class Upload;
   }
